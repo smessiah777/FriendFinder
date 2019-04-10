ef0a72ec5d984d6699f939bbaed5113f61065038
roninTDK <smessiah17@gmail.com>
2019-04-10 12:08:02 -0500

    server.js file updated

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(express.static("./app/public"));
 require("./app/routes/apiRoutes")(app);
 require("./app/routes/htmlRoutes")(app);
 
